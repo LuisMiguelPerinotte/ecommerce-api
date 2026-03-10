9f9d4bb8b2eeb6b11a4824d6b15851123d3e5520
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-10 19:14:48 -0300

    feat: adição do SwaggerConfig para documentação dos endpoints da api.

---
+++ src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.java.luismiguel.ecommerce_api.infrastructure.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-Commerce API")
+                        .description("API de E-Commerce com Spring Boot e Stripe")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
