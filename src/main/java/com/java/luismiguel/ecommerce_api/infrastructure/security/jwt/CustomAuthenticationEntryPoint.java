f77cea265527c1706ee025946354b1248e2aaa9d
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-10 19:12:09 -0300

    fix: adicionando a anotação @Component no CustomAuthenticationEntryPoint que impossibilitava o uso.

--- src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/security/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/security/jwt/CustomAuthenticationEntryPoint.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(
