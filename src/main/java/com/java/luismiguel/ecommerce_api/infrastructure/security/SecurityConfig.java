5fadd81fc8c0bd8676f3baf30dc39a96ee62ac67
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-10 16:53:14 -0300

    feat: deixando endpoint /auth/refresh publico (tinha esquecido).

--- src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
