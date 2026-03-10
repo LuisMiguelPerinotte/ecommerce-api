0110423bc9cb6064ab242f0e9977dcad6abbf8cb
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-10 19:24:12 -0300

    feat(db): V1 da migration inicial, que cria tabelas para as entidades User e Address.

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,42 @@
+--Table users
+CREATE TABLE users(
+    user_id UUID PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_role VARCHAR(50) NOT NULL,
+    active BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT chk_user_role CHECK (user_role IN ('CUSTOMER', 'ADMIN'))
+);
+
+--Update timestamp function -- Atualiza updated_at com a data atual sempre que table users for modificada.
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at();
+
+
+--Table addresses
+CREATE TABLE addresses(
+    address_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    complement VARCHAR(255),
+    state VARCHAR(2) NOT NULL,
+    zip_code VARCHAR(50) NOT NULL,
+    country VARCHAR(2) NOT NULL,
+    is_default BOOLEAN NOT NULL,
+
+    CONSTRAINT fk_address_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
