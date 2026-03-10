8a80f9ef0e31ac519dda9af93b5b7fd73fb43362
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-10 19:21:10 -0300

    feat: nova classe UserService que implementa UserDetailsService.

---
+++ src/main/java/com/java/luismiguel/ecommerce_api/application/user/UserService.java
@@ -0,0 +1,23 @@
+package com.java.luismiguel.ecommerce_api.application.user;
+
+import com.java.luismiguel.ecommerce_api.domain.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // "loadUserByUsername" é um nome fixo do Spring, mas estou usando email para carregar o usuário.
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found!"));
+    }
+}
