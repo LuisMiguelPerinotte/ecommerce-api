af372adef1b8a3cad4d2152d330d941a200f66b2
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-11 18:25:44 -0300

    feat: adição do AuthService com as regras de negócio e adição do AuthController com os endpoints.

---
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/controller/AuthController.java
@@ -0,0 +1,94 @@
+package com.java.luismiguel.ecommerce_api.api.controller;
+
+import com.java.luismiguel.ecommerce_api.api.dto.request.ChangePasswordRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.LoginRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.RefreshRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.RegisterRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.AuthResponseDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.UserResponseDTO;
+import com.java.luismiguel.ecommerce_api.application.auth.AuthService;
+import com.java.luismiguel.ecommerce_api.domain.user.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "Auth", description = "Responsável por gerenciar as requisições de autenticação de usuários.")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    @Operation(summary = "Register (POST)", description = "Valida os dados e registra um novo usuário no sistema.")
+    public ResponseEntity<UserResponseDTO> registerUser(
+            @Valid
+            @RequestBody RegisterRequestDTO registerRequestDTO
+    ) {
+        return new ResponseEntity<>(authService.registerNewUser(registerRequestDTO), HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/login")
+    @Operation(summary = "Login (POST)", description = "Realiza o login do usuário, valida as credenciais e gera um token JWT se a autenticação for bem-sucedida." )
+    public ResponseEntity<AuthResponseDTO> loginUser(
+            @Valid
+            @RequestBody LoginRequestDTO loginRequestDTO
+    ) {
+        return new ResponseEntity<>(authService.userLogin(loginRequestDTO), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/refresh")
+    @Operation(summary = "refresh token (POST)", description = "Renova o token com um refreshToken válido")
+    public ResponseEntity<AuthResponseDTO> refreshLoginToken(
+            @Valid
+            @RequestBody RefreshRequestDTO refreshRequestDTO
+    ) {
+        System.out.println("Chegou no controller");
+        return new ResponseEntity<>(authService.refreshToken(refreshRequestDTO), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @Operation(summary = "get logged user (GET)", description = "Retorna dados do usuário logado")
+    public ResponseEntity<UserResponseDTO> getLoggedUser(@AuthenticationPrincipal User user) {
+        return new ResponseEntity<>(authService.loggedUser(user), HttpStatus.OK);
+    }
+
+
+    @PostMapping("/change-password")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @Operation(summary = "Change password (POST)", description = "Muda a senha (Precisa da senha Atual)")
+    public ResponseEntity<Void> changeUserPassword(
+            @Valid
+            @RequestBody ChangePasswordRequestDTO changePasswordRequestDTO,
+            @AuthenticationPrincipal User user
+    ) {
+        authService.changePassword(changePasswordRequestDTO, user.getEmail());
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PostMapping("/logout")
+    @Operation(summary = "User Logout (POST)", description = "Inválida o refresh token atual!")
+    public ResponseEntity<Void> userLogout(
+            @AuthenticationPrincipal User user,
+            HttpServletRequest request
+    ) {
+        String accessToken = request.getHeader("Authorization").substring(7);
+        authService.logout(user.getEmail(), accessToken);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/java/luismiguel/ecommerce_api/application/auth/AuthService.java
@@ -0,0 +1,167 @@
+package com.java.luismiguel.ecommerce_api.application.auth;
+
+import com.java.luismiguel.ecommerce_api.api.dto.request.ChangePasswordRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.LoginRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.RefreshRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.RegisterRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.AuthResponseDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.UserResponseDTO;
+import com.java.luismiguel.ecommerce_api.domain.user.User;
+import com.java.luismiguel.ecommerce_api.domain.user.UserRepository;
+import com.java.luismiguel.ecommerce_api.domain.user.enums.UserRole;
+import com.java.luismiguel.ecommerce_api.infrastructure.exception.auth.*;
+import com.java.luismiguel.ecommerce_api.infrastructure.security.jwt.JwtProperties;
+import com.java.luismiguel.ecommerce_api.infrastructure.security.jwt.JwtService;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final JwtProperties jwtProperties;
+    private final RefreshTokenService refreshTokenService;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService, JwtProperties jwtProperties, RefreshTokenService refreshTokenService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.jwtProperties = jwtProperties;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    public UserResponseDTO registerNewUser(RegisterRequestDTO registerRequestDTO) {
+        User user = User.builder()
+                .email(registerRequestDTO.email().toLowerCase().trim())
+                .username(registerRequestDTO.username().trim())
+                .password(passwordEncoder.encode(registerRequestDTO.password()))
+                .userRole(UserRole.CUSTOMER)
+                .active(Boolean.TRUE)
+                .build();
+
+        try {
+            User savedUser = userRepository.save(user);
+            return new UserResponseDTO(
+                    savedUser.getUserId(),
+                    savedUser.getUsername(),
+                    savedUser.getEmail(),
+                    savedUser.getUserRole(),
+                    savedUser.getCreatedAt()
+            );
+
+        } catch (DataIntegrityViolationException exception) {
+            throw new UserEmailAlreadyRegisteredException();
+        }
+
+    }
+
+
+    public AuthResponseDTO userLogin(LoginRequestDTO loginRequestDTO) {
+        try{
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    loginRequestDTO.email(),
+                    loginRequestDTO.password()
+            );
+
+            Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+            User user = (User) authentication.getPrincipal();
+            String accessToken = jwtService.generateToken(user);
+            String refreshToken = jwtService.generateRefreshToken(user);
+
+            refreshTokenService.saveRefreshToken(user.getUserId(), refreshToken);
+
+            return new AuthResponseDTO(
+                    accessToken,
+                    refreshToken,
+                    "Bearer",
+                    jwtProperties.getExpiration() / 1000
+            );
+
+        } catch (BadCredentialsException e) {
+            throw new InvalidCredentialsException();
+        }
+
+    }
+
+
+    public AuthResponseDTO refreshToken(RefreshRequestDTO refreshRequestDTO) {
+        System.out.println("Chegou no service");
+        String email = jwtService.validateRefreshToken(refreshRequestDTO.refreshToken());
+
+        System.out.println("passou pela validação do jwtservice");
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        System.out.println("passou pela validação de usuario");
+
+        if (!refreshTokenService.isValid(user.getUserId(), refreshRequestDTO.refreshToken())) {
+            System.out.println("não passou pela verificação se o token é valido");
+            throw new InvalidRefreshTokenException();
+        }
+        System.out.println("passou pela verificação se o token é valido");
+
+        String newRefreshToken = jwtService.generateRefreshToken(user);
+        System.out.println("gerou refresh token");
+
+        refreshTokenService.saveRefreshToken(user.getUserId(), newRefreshToken);
+        System.out.println("salvou refresh token no redis");
+
+        return new AuthResponseDTO(
+                jwtService.generateToken(user),
+                newRefreshToken,
+                "Bearer",
+                jwtProperties.getExpiration() / 1000
+        );
+    }
+
+
+    public UserResponseDTO loggedUser(User user){
+        return new UserResponseDTO(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getUserRole(),
+                user.getCreatedAt()
+        );
+    }
+
+
+    public void changePassword(ChangePasswordRequestDTO changePasswordRequestDTO, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        if (!passwordEncoder.matches(changePasswordRequestDTO.currentPassword(), user.getPassword())) {
+            throw new InvalidPasswordException();
+        }
+
+        if (passwordEncoder.matches(changePasswordRequestDTO.newPassword(), user.getPassword())) {
+            throw new PasswordUnchangedException();
+        }
+
+        user.setPassword(passwordEncoder.encode(changePasswordRequestDTO.newPassword()));
+        userRepository.save(user);
+    }
+
+
+    public void logout(String email, String accessToken) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        refreshTokenService.deleteRefreshToken(user.getUserId());
+
+        refreshTokenService.blackListAccessToken(
+                accessToken,
+                jwtProperties.getExpiration()
+        );
+    }
+}
