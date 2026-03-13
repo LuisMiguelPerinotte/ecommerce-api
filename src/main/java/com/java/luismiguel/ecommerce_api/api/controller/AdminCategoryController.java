e6336a2864fa7ea8d9ce468b05e1fb153e56cab1
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-13 14:23:14 -0300

    feat: adicionando Controllers Admin e publico para manipulação das categorias.

---
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/controller/AdminCategoryController.java
@@ -0,0 +1,58 @@
+package com.java.luismiguel.ecommerce_api.api.controller;
+
+import com.java.luismiguel.ecommerce_api.api.dto.request.CreateCategoryRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.request.UpdateCategoryRequestDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.CreatedCategoryResponseDTO;
+import com.java.luismiguel.ecommerce_api.application.category.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/categories")
+@Tag(name = "Admin Category", description = "Operações de admin nas Categorias.")
+public class AdminCategoryController {
+    private final CategoryService categoryService;
+
+    public AdminCategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "create category (POST)", description = "Cria uma categoria com as informações do request.")
+    public ResponseEntity<CreatedCategoryResponseDTO> createCategory(
+            @Valid
+            @RequestBody CreateCategoryRequestDTO createCategoryRequestDTO
+    ) {
+        return new ResponseEntity<>(categoryService.createCategory(createCategoryRequestDTO), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "edit category (PUT)", description = "edita uma categoria pelo id dado como parâmetro.")
+    public ResponseEntity<Void> editCategory(
+            @Valid
+            @PathVariable UUID id,
+            @RequestBody UpdateCategoryRequestDTO updateCategoryRequestDTO
+    ) {
+        categoryService.updateCategory(id, updateCategoryRequestDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "delete category (DELETE)", description = "faz um soft delete na categoria com o id dado como parâmetro.")
+    public ResponseEntity<Void> softDeleteCategory(@PathVariable UUID id) {
+        categoryService.softDeleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/controller/CategoryController.java
@@ -0,0 +1,40 @@
+package com.java.luismiguel.ecommerce_api.api.controller;
+
+import com.java.luismiguel.ecommerce_api.api.dto.response.GetCategoryResponseDTO;
+import com.java.luismiguel.ecommerce_api.api.dto.response.GetAllActiveCategoriesDTO;
+import com.java.luismiguel.ecommerce_api.application.category.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/categories")
+@Tag(name = "Category", description = "Opções de Get das Categorias.")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    @Operation(summary = "get all categories (GET)", description = "retorna todas as categorias em paginas e em número de itens escolidos.")
+    public ResponseEntity<List<GetAllActiveCategoriesDTO>> getAllCategories(
+            @RequestParam Integer page,
+            @RequestParam Integer size
+    ) {
+        return new ResponseEntity<>(categoryService.getAllCategories(page, size), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    @Operation(summary = "get category (GET)", description = "retorna uma categoria especifica pelo id dela.")
+    public ResponseEntity<GetCategoryResponseDTO> getCategoryById(@PathVariable UUID id) {
+        return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
+    }
+}
