462c0b39b0ddfdadab5777171228adecb7256448
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-13 14:21:35 -0300

    feat: DTOs para request e response do endpoints de categoria.

---
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/dto/response/GetAllActiveCategoriesDTO.java
@@ -0,0 +1,13 @@
+package com.java.luismiguel.ecommerce_api.api.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record GetAllActiveCategoriesDTO(
+        UUID categoryId,
+        String name,
+        String slug,
+        LocalDateTime createdAt
+
+) {
+}
