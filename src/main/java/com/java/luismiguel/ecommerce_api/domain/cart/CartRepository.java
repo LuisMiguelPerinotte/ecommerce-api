07bd04854c5d3806dccbc331048731149cb246a2
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-03-23 20:06:46 -0300

    feat: adicionando novo método de busca com query ao CartRepository

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/cart/CartRepository.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/cart/CartRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,4 +15,7 @@ public interface CartRepository extends JpaRepository<Cart, UUID> {
     @Query("SELECT SUM(ci.quantity) as totalItems, SUM(ci.subtotal) as totalAmount " +
             "FROM CartItem ci WHERE ci.cart.id = :cartId")
     CartSummary getCartSummary(@Param("cartId") UUID cartId);
+
+    @Query("SELECT c FROM Cart c LEFT JOIN FETCH c.items i LEFT JOIN FETCH i.product WHERE c.user.userId = :userId")
+    Optional<Cart> findByUserUserIdWithItems(@Param("userId") UUID userId);
 }
