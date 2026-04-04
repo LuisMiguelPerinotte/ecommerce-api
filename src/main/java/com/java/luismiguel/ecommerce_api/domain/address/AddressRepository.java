02e48d65bfae762c653e6e530ce73c9bf5d1f280
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-04 11:15:47 -0300

    feat: adicionando mais métodos no AddressRepository

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/address/AddressRepository.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/address/AddressRepository.java
@@ -1,10 +1,18 @@
 package com.java.luismiguel.ecommerce_api.domain.address;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+    Page<Address> findByUserUserIdAndActiveTrue(UUID userId, Pageable pageable);
+
+    Optional<Address> findByUserUserIdAndStreetAndHouseNumber(UUID userId, String street, String houseNumber);
+
+    Optional<Address> findByUserUserIdAndIsDefaultTrue(UUID userId);
 }
