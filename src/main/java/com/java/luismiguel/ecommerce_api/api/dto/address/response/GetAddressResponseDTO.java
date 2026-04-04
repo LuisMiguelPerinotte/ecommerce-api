868def59aacc9d52cf44d643ca87b045fb2e9073
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-04 11:20:35 -0300

    feat: modificando DTO de GetAddressResponseDTO

--- src/main/java/com/java/luismiguel/ecommerce_api/api/dto/address/response/GetAddressResponseDTO.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/dto/address/response/GetAddressResponseDTO.java
@@ -1,13 +1,16 @@
-package com.java.luismiguel.ecommerce_api.api.dto.response;
+package com.java.luismiguel.ecommerce_api.api.dto.address.response;
 
 import java.util.UUID;
 
 public record GetAddressResponseDTO(
         UUID addressId,
         String street,
+        String number,
         String complement,
+        String neighborhood,
+        String city,
         String state,
         String zipCode,
-        String country
+        Boolean isDefault
 ) {
 }
