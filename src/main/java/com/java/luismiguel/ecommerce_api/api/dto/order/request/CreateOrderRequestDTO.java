4748855175df847c0a587e41a5ecdbd8dc601a27
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-04 11:21:14 -0300

    feat: deixando campo addressId do DTO CreateOrderRequestDTO opcional.

--- src/main/java/com/java/luismiguel/ecommerce_api/api/dto/order/request/CreateOrderRequestDTO.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/api/dto/order/request/CreateOrderRequestDTO.java
@@ -1,12 +1,10 @@
 package com.java.luismiguel.ecommerce_api.api.dto.order.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record CreateOrderRequestDTO(
-        @NotNull
         UUID addressId,
 
         @Size(max = 200)
