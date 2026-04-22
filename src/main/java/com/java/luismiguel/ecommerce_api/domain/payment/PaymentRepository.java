4bbfe7ebc2bfef835398bff1315744f93ead3122
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-22 15:51:32 -0300

    feat: adicionando métodos para busca de dados, auxiliando PaymentService

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/payment/PaymentRepository.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/payment/PaymentRepository.java
@@ -1,7 +1,12 @@
 package com.java.luismiguel.ecommerce_api.domain.payment;
 
 import com.java.luismiguel.ecommerce_api.domain.order.Order;
+import com.java.luismiguel.ecommerce_api.domain.payment.enums.PaymentStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +14,24 @@ import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-    Optional<Payment> findByOrder(Order order);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        SELECT p FROM Payment p
+        JOIN FETCH p.order
+        WHERE p.stripeSessionId = :id
+""")
+    Optional<Payment> findByStripeSessionIdWithOrder(@Param("id") String stripeSessionId);
 
-    Boolean existsByMpPaymentId(String mpPaymentId);
+    @Query("""
+    SELECT p FROM Payment p
+    JOIN FETCH p.order o
+    WHERE o.orderId = :orderId
+    AND p.status = :status
+""")
+    Optional<Payment> findPaymentByOrderIdAndStatusWithOrder(
+            UUID orderId,
+            PaymentStatus status
+    );
+
+    Boolean existsByOrderAndStatus(Order order, PaymentStatus status);
 }
