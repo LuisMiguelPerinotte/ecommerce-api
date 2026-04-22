d1ff3d496e9991bbe09d7f78ce77374f908acc64
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-22 15:35:05 -0300

    feat: adicionando enum PAYMENT_FAILED no OrderStatus

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/order/enums/OrderStatus.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/order/enums/OrderStatus.java
@@ -3,6 +3,7 @@ package com.java.luismiguel.ecommerce_api.domain.order.enums;
 public enum OrderStatus {
     PENDING,
     AWAITING_PAYMENT,
+    PAYMENT_FAILED,
     PAID,
     PROCESSING,
     SHIPPED,
