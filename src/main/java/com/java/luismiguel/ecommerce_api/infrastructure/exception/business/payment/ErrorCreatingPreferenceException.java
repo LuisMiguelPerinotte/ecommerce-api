4e7b7eed345af4b22e6089f9f1349d1e7a46f96c
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-22 15:30:04 -0300

    remove: removendo ErrorCreatingPreferenceException por inutilização

--- src/main/java/com/java/luismiguel/ecommerce_api/infrastructure/exception/business/payment/ErrorCreatingPreferenceException.java
+++
@@ -1,9 +0,0 @@
-package com.java.luismiguel.ecommerce_api.infrastructure.exception.business.payment;
-
-import org.springframework.http.HttpStatus;
-
-public class ErrorCreatingPreferenceException extends PaymentException {
-    public ErrorCreatingPreferenceException() {
-        super("Error Creating Preference!", HttpStatus.BAD_GATEWAY);
-    }
-}
