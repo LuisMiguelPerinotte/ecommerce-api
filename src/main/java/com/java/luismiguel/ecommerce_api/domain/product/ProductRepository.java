2b59d9ed652f8b3731be0bb2bafe1b80b3fcad4d
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-22 15:37:57 -0300

    feat: adicionando métodos necessários para auxiliar o resgate de dados do banco para o admin dashboard

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/product/ProductRepository.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/product/ProductRepository.java
@@ -2,10 +2,31 @@ package com.java.luismiguel.ecommerce_api.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+    @Query("""
+        SELECT p.productId as productId,
+               p.name as name,
+               p.stockQuantity as stockQuantity
+        FROM Product p
+        WHERE p.stockQuantity < :threshold
+            AND p.active = true
+""")
+    List<LowStockProductProjection> findLowStockProducts(@Param("threshold") Integer threshold);
+
+
+    @Query("""
+        SELECT COUNT(p)
+        FROM Product p
+        WHERE p.stockQuantity < :threshold
+            AND p.active = true
+""")
+    Integer countLowStockProducts(@Param("threshold") Integer threshold);
 }
