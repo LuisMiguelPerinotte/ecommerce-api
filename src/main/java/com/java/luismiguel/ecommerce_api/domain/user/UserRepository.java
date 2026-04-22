b760a044537263f66d2aa4cd0becb244d103a4f3
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-04-22 15:31:38 -0300

    feat: adicionando métodos no UserRepository para buscar todos os usuários (Paginado), e um para contar quantos usuários ativos

--- src/main/java/com/java/luismiguel/ecommerce_api/domain/user/UserRepository.java
+++ src/main/java/com/java/luismiguel/ecommerce_api/domain/user/UserRepository.java
@@ -1,10 +1,23 @@
 package com.java.luismiguel.ecommerce_api.domain.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+
+    Page<User> findAll(Pageable pageable);
+
+    @Query("""
+        SELECT COUNT(u)
+        FROM User u
+        WHERE u.userRole = 'ROLE_CUSTOMER'
+            AND u.active = true
+""")
+    Long countActiveCustomers();
 }
