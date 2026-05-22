6d321f0c02cc66f4289b9a664beac932ff742b85
LuísMiguelPerinotte <luizmiguel0010m@gmail.com>
2026-05-22 12:37:46 -0300

    test: configure test profile for application context

--- src/test/java/com/java/luismiguel/ecommerce_api/EcommerceApiApplicationTests.java
+++ src/test/java/com/java/luismiguel/ecommerce_api/EcommerceApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.java.luismiguel.ecommerce_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcommerceApiApplicationTests {
 
 	@Test
